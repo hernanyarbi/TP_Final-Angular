96e2fd9a92cc9f4ced6abc1be3f1f71f115b08ad
Unknown <joedzcell@gmail.com>
2018-07-01 13:41:00 -0300

    se agrego vista home y visitante

--- app/components/visitante/visitante.component.ts
+++ app/components/visitante/visitante.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalService } from '../../services/local-service';
 
 @Component({
   selector: 'app-visitante',
   templateUrl: './visitante.component.html',
-  styleUrls: ['./visitante.component.css']
+  styleUrls: ['./visitante.component.css'],
+  providers: [ LocalService ]
 })
-export class VisitanteComponent implements OnInit {
 
-  constructor() { }
+export class VisitanteComponent implements OnInit {
+  locales = [];
+  constructor(private servicio:LocalService) {
+    this.cargarLocales()
+  }
 
   ngOnInit() {
+  }
+
+  cargarLocales(){
+    this.servicio.getLocals().subscribe(
+      result=>{
+        this.locales = JSON.parse(result.locals);
+        console.log(this.locales);
+      },
+      error =>{
+        console.error(error);
+      }
+    );
   }
 
 }
